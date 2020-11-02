90dadd7dc037193c150b4e28b1c195722b2fa575
Pontus REDIG <pontus.redig@alten.se>
2020-11-02 11:53:07 +0100

    Initial commit

---
+++ src/main/java/se/iths/service/ItemService.java
@@ -0,0 +1,34 @@
+package se.iths.service;
+
+import se.iths.entity.Item;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Transactional
+public class ItemService {
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    public Item createItem(Item item) {
+        entityManager.persist(item);
+        return item;
+    }
+
+    public Item updateItem(Item item) {
+        entityManager.merge(item);
+        return item;
+    }
+
+    public Item findItemById(Long id) {
+        return entityManager.find(Item.class, id);
+    }
+
+    public List<Item> getAllItems() {
+        return entityManager.createQuery("SELECT i from Item i", Item.class).getResultList();
+    }
+
+}
