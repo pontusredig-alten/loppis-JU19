5de837f727181bd4fa83ee0170de4b715822e932
Pontus Redig <pontus.redig@alten.se>
2020-11-25 16:10:22 +0100

    Change to BuyerService

--- src/main/java/se/iths/service/BuyerService.java
+++ src/main/java/se/iths/service/BuyerService.java
@@ -1,14 +1,11 @@
 package se.iths.service;
 
-import se.iths.entity.Buyer;
 import se.iths.entity.Item;
 import se.iths.entity.User;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Set;
 
 @Transactional
